3df5091b0c298083a9cf29f8eac73dddae4c4758
Moe Finkelstein <mfinkelstein85@gmail.com>
2019-05-09 15:02:36 -0400

    Add project files.

---
+++ Class62Homework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Class62Homework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
